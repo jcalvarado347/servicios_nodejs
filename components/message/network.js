10b7977d9e88c66733ff5acd62aa3fa1a47faeba
jcalvarado347 <jcalvarado347@gmail.com>
2023-09-23 23:02:59 -0400

    Mi primer commit

---
+++ components/message/network.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const response = require('../../network/response');
+const controller = require('./controller');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    controller.getMessages()
+        .then((messageList) => {
+            response.success(req, res, messageList, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Unexpected Error', 500, e);
+        })
+});
+
+router.post('/', function (req, res) {
+
+    controller.addMessage(req.body.user, req.body.message)
+        .then((fullMessage) => {
+            response.success(req, res, fullMessage, 201);
+        })
+        .catch(e => {
+            response.error(req, res, 'Información invalida', 400, 'Error en controllador');
+        });
+
+});
+
+module.exports = router;
